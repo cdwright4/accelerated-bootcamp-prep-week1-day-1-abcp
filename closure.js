1d5d98fbee39115e0d111c6d4ff87bb54f0818b8
Crystal <crystal_wright@yahoo.com>
2018-01-13 22:27:52 +0000

    automatically backed up by learn

--- closure.js
+++ closure.js
@@ -1,16 +1,16 @@
 function parent() {
   var innerVariable = "Go to your room!";
-  console.log(inaccessible)
+  //console.log(inaccessible)
   return function childe() {
     var inaccessible = "**sneaks out the bedroom window**";
     return innerVariable;
   }
 }
 
-var outerScope = parent;
+//var outerScope = parent;
 
-var innerScope = outerScope();
+//var innerScope = outerScope();
 
-innerScope();
+//innerScope();
 
 
